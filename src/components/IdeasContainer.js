48522dbc8c5a5dee396f75879e91c1ef888cb023
Spencer Brooks <spencer@databit.io>
2018-12-22 12:36:41 -0700

    add new cards

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import Idea from './Idea'
+import update from 'immutability-helper'
 
 class IdeasContainer extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      ideas: []
+      ideas: [],
+      editingIdeaId: null,
     }
   }
 
@@ -20,10 +22,34 @@ class IdeasContainer extends Component {
     .catch(error => console.log(error))
   }
 
+  addNewIdea = () => {
+    axios.post(
+      'http://localhost:3001/api/v1/ideas',
+      { idea:
+        {
+          title: '',
+          body: ''
+        }
+      }
+    )
+    .then(response => {
+      const ideas = update(this.state.ideas, {
+        $splice: [[0, 0, response.data]]
+      })
+      this.setState({
+        ideas: ideas,
+        editingIdeaId: response.data.id
+      })
+    })
+    .catch(error => console.log(error))
+  }
+
+
+
   render() {
     return (
       <>
-        <button className='newIdeaButton'>
+        <button className='newIdeaButton' onClick={this.addNewIdea}>
           New Idea
         </button>
         <div>
