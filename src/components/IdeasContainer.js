6e001ef1987bffe40a3c71986aa7f6b530cc7ac8
Spencer Brooks <spencer@databit.io>
2018-12-21 21:58:24 -0700

    add state to ideaboard

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class IdeasContainer extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      ideas: []
+    }
+  }
+
+  componentDidMount() {
+    axios('http://localhost:3001/api/v1/ideas.json')
+    .then(response => {
+      console.log(response)
+      this.setState({ideas: response.data})
+    })
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div>
