96db3991261d63dba999a19334df6351d29abdb3
Spencer Brooks <spencer@databit.io>
2018-12-22 22:47:35 -0700

    add ideaform comp

---
+++ src/components/IdeaForm.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class IdeaForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: this.props.idea.title,
+      body: this.props.idea.body,
+    }
+  }
+
+  render() {
+    return(
+      <div className="tile">
+        <form>
+          <input type="text" className="input" name='title' placeholder='Enter a Title' value={this.state.title} onChange={this.handleInput} />
+          <textarea name="body" className="input" placeholder='Describe Your Idea' value={this.state.body} onChange={this.handleInput}></textarea>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default IdeaForm
