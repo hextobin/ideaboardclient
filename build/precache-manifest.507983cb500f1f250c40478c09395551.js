37b29122d8092ed7590f13c72c3ce9cde534a45e
Spencer Brooks <spencer@databit.io>
2018-12-30 22:03:38 -0700

    built

---
+++ build/precache-manifest.507983cb500f1f250c40478c09395551.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b0e79f6168242c534719",
+    "url": "/static/js/main.b0e79f61.chunk.js"
+  },
+  {
+    "revision": "c450ff4d9a1ef880faca",
+    "url": "/static/js/1.c450ff4d.chunk.js"
+  },
+  {
+    "revision": "b0e79f6168242c534719",
+    "url": "/static/css/main.cb2c2c2e.chunk.css"
+  },
+  {
+    "revision": "3aaf253166fb9bb354f3b20cdb146734",
+    "url": "/index.html"
+  }
+];
