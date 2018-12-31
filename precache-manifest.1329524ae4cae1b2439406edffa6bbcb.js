c2519002960f6410744dd5e7260932a255fb8d05
Spencer Brooks <spencer@databit.io>
2018-12-31 10:20:08 -0700

    Updates

--- precache-manifest.1329524ae4cae1b2439406edffa6bbcb.js
+++ precache-manifest.1329524ae4cae1b2439406edffa6bbcb.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/ideaboardclient/static/js/runtime~main.e7ef5bdf.js"
   },
   {
-    "revision": "be64fea33d1397ed0502",
-    "url": "/ideaboardclient/static/js/main.be64fea3.chunk.js"
+    "revision": "7dc4e54ca3b51f459989",
+    "url": "/ideaboardclient/static/js/main.7dc4e54c.chunk.js"
   },
   {
     "revision": "c450ff4d9a1ef880faca",
     "url": "/ideaboardclient/static/js/1.c450ff4d.chunk.js"
   },
   {
-    "revision": "be64fea33d1397ed0502",
+    "revision": "7dc4e54ca3b51f459989",
     "url": "/ideaboardclient/static/css/main.cb2c2c2e.chunk.css"
   },
   {
-    "revision": "f17beb9fa6b9ca3066051d4f14e9afc7",
+    "revision": "54c0c2c20130abed12e9a76b22b55e2b",
     "url": "/ideaboardclient/index.html"
   }
 ];
