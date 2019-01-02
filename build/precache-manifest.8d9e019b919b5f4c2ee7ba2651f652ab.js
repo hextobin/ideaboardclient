ae1e4f80da0836f673b394961c8a1b99693df305
Spencer Brooks <spencer@databit.io>
2019-01-01 20:04:12 -0700

    some css

---
+++ build/precache-manifest.8d9e019b919b5f4c2ee7ba2651f652ab.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/ideaboardclient/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "670fe487c1791d4a3fdb",
+    "url": "/ideaboardclient/static/css/main.7d58824c.chunk.css"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/ideaboardclient/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "a32d96b5c3b1d9e7af9d",
+    "url": "/ideaboardclient/static/js/1.a32d96b5.chunk.js"
+  },
+  {
+    "revision": "e7ef5bdfdfee9ed2b5e4",
+    "url": "/ideaboardclient/static/js/runtime~main.e7ef5bdf.js"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/ideaboardclient/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/ideaboardclient/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/ideaboardclient/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/ideaboardclient/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/ideaboardclient/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/ideaboardclient/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "670fe487c1791d4a3fdb",
+    "url": "/ideaboardclient/static/js/main.670fe487.chunk.js"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/ideaboardclient/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/ideaboardclient/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/ideaboardclient/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/ideaboardclient/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/ideaboardclient/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/ideaboardclient/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/ideaboardclient/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/ideaboardclient/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a32d96b5c3b1d9e7af9d",
+    "url": "/ideaboardclient/static/css/1.e9d2e907.chunk.css"
+  },
+  {
+    "revision": "5e4f82794c5d18acb67cd5a00bb95e91",
+    "url": "/ideaboardclient/index.html"
+  }
+];
